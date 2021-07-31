606d236169216f0b73b8082c2d51daa8db2a4754
justhungryman <justhungryman@gmail.com>
2021-07-31 22:14:32 +0900

    [COMMIT] optimizer test

--- baseline/optimizer/__init__.py
+++ baseline/optimizer/__init__.py
@@ -3,11 +3,31 @@ import tensorflow as tf
 
 LOGGER = logging.getLogger(__name__)
 
-def create(conf):
-    optimizer = tf.keras.optimizers.get({
-        "class_name": conf['type'],
-        "config": conf['params']
-        })
+# def create(conf):
+#     optimizer = tf.keras.optimizers.get({
+#         "class_name": conf['type'],
+#         "config": conf['params']
+#         })
         
-    LOGGER.info(f'[Optimizer] create {conf["type"]}')
-    return optimizer
+#     LOGGER.info(f'[Optimizer] create {conf["type"]}')
+#     return optimizer
+
+def create(config):
+    opt_type = config['type'].lower()
+    if opt_type == 'sgd':
+        LOGGER.info(f'[optimizer] create {opt_type}')
+        return tf.keras.optimizers.SGD(**config['params'])
+    elif opt_type == 'sgdw':
+        LOGGER.info(f'[optimizer] create {opt_type}')
+        return tfa.optimizers.SGDW(**config['params'])
+    elif opt_type == 'adam':
+        LOGGER.info(f'[optimizer] create {opt_type}')
+        return tf.keras.optimizers.Adam()
+    elif opt_type == 'adamw':
+        LOGGER.info(f'[optimizer] create {opt_type}')
+        return tfa.optimizers.AdamW(**config['params'])
+    elif opt_type == 'lamb':
+        LOGGER.info(f'[optimizer] create {opt_type}')
+        return tfa.optimizers.LAMB(**config['params'])
+
+    raise AttributeError(f'not support optimizer config: {config}')
